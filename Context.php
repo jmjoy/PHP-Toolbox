6b2cdbbaf721878149db9e8cd046ddc18b676dd3
jmjoy <918734043@qq.com>
2015-04-19 17:26:47 +0800

    Context: just a outline

--- Context.php
+++ Context.php
@@ -3,37 +3,37 @@
 class Context {
 
     public static function get($key=null, $default=null) {
-        return $this->sgArrValue($_GET, $key, $default);
+        return self::sgArrValue($_GET, $key, $default);
     }
 
     public static function post($key=null, $default=null) {
-        return $this->sgArrValue($_POST, $key, $default);
+        return self::sgArrValue($_POST, $key, $default);
     }
 
     public static function server($key=null, $default=null) {
-        return $this->sgArrValue($_SERVER, $key, $default);
+        return self::sgArrValue($_SERVER, $key, $default);
     }
 
     public static function cookie($key=null, $default=null) {
-        return $this->sgArrValue($_COOKIE, $key, $default);
+        return self::sgArrValue($_COOKIE, $key, $default);
     }
 
     public static function session($key=null, $default=null) {
         if (!isset($_SESSION)) {
             session_start();
         }
-        return $this->sgArrValue($_SESSION, $key, $default);
+        return self::sgArrValue($_SESSION, $key, $default);
     }
 
     public static function flashCookie($key, $default=null) {
-        $cookie = $this->cookie($key, $default);
-        unset($_COOKIE[$key]);
+        $cookie = self::cookie($key, $default);
+        self::delCookie($key);
         return $cookie;
     }
 
     public static function flashSession($key, $default=null) {
-        $session = $this->session($key, $default);
-        unset($_SESSION[$key]);
+        $session = self::session($key, $default);
+        self::delFromArr($_SESSION, $key);
         return $session;
     }
 
@@ -47,14 +47,101 @@ class Context {
         return setcookie($key, $value, time()+$time);
     }
 
-    protected static function sgArrValue($sgArr, $key, $default) {
+    public static function setSession($key, $value) {
+
+    }
+
+    public static function delCookie($key) {
+
+    }
+
+    public static function delSession($key) {
+
+    }
+
+    protected static function getFromArr(&$arr, $key, $default) {
         if ($key === null) {
-            return $sgArr;
+            return $arr;
         }
-        if (isset($sgArr[$key])) {
-            return $sgArr[$key];
+
+        if (!is_array($key)) {
+            if (!isset($arr[$key])) {
+                return $default;
+            }
+            return $arr[$key];
         }
-        return $default;
+
+        switch (count($key)) {
+        case 0:
+            return $default;
+
+        case 1:
+            if (!isset($arr[$key])) {
+                return $default;
+            }
+            return $arr[$key];
+
+        case 2:
+            list($k0, $k1) = $key;
+            if (!isset($arr[$k0][$k1])) {
+                return $default;
+            }
+            return $arr[$k0][$k1];
+
+        case 3:
+            list($k0, $k1, $k2) = $key;
+            if (!isset($arr[$k0][$k1][$k2])) {
+                return $default;
+            }
+            return $arr[$k0][$k1][$k2];
+
+        case 4:
+            list($k0, $k1, $k2, $k3) = $key;
+            if (!isset($arr[$k0][$k1][$k2][$k3])) {
+                return $default;
+            }
+            return $arr[$k0][$k1][$k2][$k3];
+
+        case 5:
+            list($k0, $k1, $k2, $k3, $k4) = $key;
+            if (!isset($arr[$k0][$k1][$k2][$k3][$k4])) {
+                return $default;
+            }
+            return $arr[$k0][$k1][$k2][$k3][$k4];
+
+        default:
+            return $default;
+        }
+    }
+
+    protected static function delFromArr(&$arr, $key) {
+        if (!is_array($key)) {
+            return unset($arr[$key]);
+        }
+
+        switch (count($key)) {
+        case 0:
+            return;
+
+        case 1:
+            return unset($arr[$key]);
+
+        case 2:
+            list($k0, $k1) = $key;
+            return unset($arr[$k0][$k1]);
+
+        case 3:
+            list($k0, $k1, $k2) = $key;
+            return unset($arr[$k0][$k1][$k2]);
+
+        case 4:
+            list($k0, $k1, $k2, $k3) = $key;
+            return unset($arr[$k0][$k1][$k2][$k3]);
+
+        case 5:
+            list($k0, $k1, $k2, $k3, $k4) = $key;
+            return unset($arr[$k0][$k1][$k2][$k3][$k4]);
+        }
     }
 
 }
